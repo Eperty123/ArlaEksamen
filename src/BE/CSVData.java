09b8bfdfef6002dcfa9bc115f3dcb689e1234e5e
Onii-chan <failer007@live.dk>
2021-04-29 15:26:44 +0200

    Implemented IFileParser interface + CSVParser + libs + BarChart mock data

---
+++ src/BE/CSVData.java
@@ -0,0 +1,111 @@
+package BE;
+
+import java.util.HashMap;
+
+/**
+ * Author: Carlo De Leon
+ * Version: 1.0.0
+ */
+public class CSVData implements IParsedData {
+    HashMap<Integer, CSVColumnData> data;
+
+    public CSVData() {
+        initialize();
+    }
+
+    /**
+     * Initialize the class. Put all variables to initialize here.
+     */
+    private void initialize() {
+        data = new HashMap<>();
+    }
+
+    /**
+     * Add a new column with value.
+     *
+     * @param columnName  The column name.
+     * @param columnValue The value.
+     */
+    public void addColumnData(String columnName, String columnValue) {
+        if (data != null) {
+            Integer index = data.size() > 0 ? data.size() + 1 : data.size();
+            data.put(index, new CSVColumnData(columnName, columnValue));
+        }
+    }
+
+    /**
+     * Get a CSVColumnData with the given column name and column value.
+     *
+     * @param columnName  The column name.
+     * @param columnValue The column value.
+     * @return Returns the found  CSVColumnData otherwise null.
+     */
+    public CSVColumnData getColumnData(String columnName, String columnValue) {
+        if (data.size() > 0) {
+            var values = data.values().toArray();
+            for (int i = 0; i < values.length; i++) {
+                var columnData = ((CSVColumnData) values[i]);
+                if (columnData.getColumnName().equals(columnName) && columnData.getColumnValue().equals(columnValue))
+                    return columnData;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get a CSVColumnData with the given line index.
+     *
+     * @param lineIndex The line index of the ColumnData.
+     * @return
+     */
+    public CSVColumnData getColumnData(int lineIndex) {
+
+        return data.get(lineIndex);
+    }
+
+    /**
+     * Does the specified column name exist?
+     *
+     * @param columnName The column name to find.
+     * @return Returns true if yes and false if not.
+     */
+    public boolean hasColumn(String columnName) {
+        if (data.size() > 0) {
+            var values = data.values().toArray();
+            for (int i = 0; i < values.length; i++) {
+                var columnData = ((CSVColumnData) values[i]);
+                return columnData.getColumnName().equals(columnName);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Does the specified CSVColumnData with the given column name and column value exist?
+     *
+     * @param columnName  The column name to find.
+     * @param columnValue The associated column value.
+     * @return Returns true if yes otherwise false.
+     */
+    public boolean hasColumnValue(String columnName, String columnValue) {
+        if (data.size() > 0) {
+            var values = data.values().toArray();
+            for (int i = 0; i < values.length; i++) {
+                var columnData = ((CSVColumnData) values[i]);
+                return columnData.getColumnName().equals(columnName) && columnData.getColumnValue().equals(columnValue);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        var values = data.values().toArray();
+        for (int i = 0; i < values.length; i++) {
+            var columnData = values[i];
+            sb.append(String.format("Linje %d\n%s\n", i, columnData.toString()));
+        }
+        return sb.toString();
+    }
+}
---
+++ src/DAL/CSVParser.java
@@ -0,0 +1,123 @@
+package DAL;
+
+import BE.CSVData;
+import BE.IParsedData;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
+import org.apache.commons.io.input.BOMInputStream;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+/**
+ * Author: Carlo De Leon
+ * Version: 1.0.0
+ */
+public class CSVParser implements IFileParser {
+    protected CSVReader csvReader;
+    protected CSVData csvData;
+    public static char delimeter = ';';
+
+    public CSVParser() {
+
+    }
+
+    public CSVParser(String file) {
+        loadFile(file);
+    }
+
+    public CSVParser(String file, char delimeter) {
+        this.delimeter = delimeter;
+        loadFile(file);
+    }
+
+    @Override
+    public void loadFile(String file) {
+        var _file = new File(file);
+
+        try {
+            var fs = new FileInputStream(_file.getPath());
+
+            // To avoid any utf bom issues we must use the BOMInputStream.
+            // https://stackoverflow.com/questions/23551683/two-identical-strings-are-not-equalnot-pointer-reference-mistake
+            // https://stackoverflow.com/questions/56189424/opencsv-csvtobean-first-column-not-read-for-utf-8-without-bom/56222034
+            // Took me a while to figure out why identical strings didn't match and that's because of said issue.
+            com.opencsv.CSVParser csvParser = new CSVParserBuilder().withSeparator(delimeter).build(); // custom separator
+            CSVReader reader = new CSVReaderBuilder(new InputStreamReader(new BOMInputStream(fs), StandardCharsets.UTF_8))
+                    .withCSVParser(csvParser)   // custom CSV parser
+                    //.withSkipLines(1)           // skip the first line, header info
+                    .build();
+
+            csvReader = reader;
+            parse();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public <T> T parse(String file) {
+        loadFile(file);
+        return parse();
+    }
+
+    @Override
+    public <T> T parse() {
+        if (csvReader != null) {
+            try {
+                var csvData = new CSVData();
+
+                var header = new ArrayList<String>();
+                var lines = csvReader.readAll();
+
+                // Parse the file if there are actual lines to read.
+                if (lines.size() > 0) {
+                    for (int i = 0; i < lines.size(); i++) {
+                        var line = lines.get(i);
+
+                    /* Get the header information.
+                    /* Index 0 is always the header information. We NEED the header in order to
+                    /* know what the values refer to what.
+                    */
+                        if (i == 0) {
+                            for (int h = 0; h < line.length; h++) {
+                                var columnName = line[h].strip().trim();
+
+                                // Add the column name.
+                                if (!header.contains(columnName)) header.add(columnName);
+                                else continue;
+                                //System.out.println(columnName);
+                            }
+                        } else {
+                            // Got the header information, now read all the column values.
+                            for (int c = 0; c < line.length; c++) {
+                                var columnValue = line[c].strip().trim();
+                                csvData.addColumnData(header.get(c).strip().trim(), columnValue);
+                            }
+                        }
+                    }
+                    this.csvData = csvData;
+                    return (T) this.csvData;
+                }
+
+            } catch (CsvValidationException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (CsvException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public IParsedData getParsedData() {
+        return csvData;
+    }
+
+}
