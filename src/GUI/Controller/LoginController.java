8c9b59722615b2e4075eb2a0977d4df1114cb127
DP <denn06g@easv365.dk>
2021-05-04 19:26:01 +0200

    fixed the timer label

--- src/GUI/Controller/LoginController.java
+++ src/GUI/Controller/LoginController.java
@@ -4,7 +4,6 @@ import BE.Timer;
 import BE.User;
 import BE.UserType;
 import BLL.LoginManager;
-import BLL.PasswordManager;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
@@ -12,7 +11,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
@@ -21,15 +20,18 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController{
     @FXML
     private JFXTextField txtUsername;
     @FXML
@@ -37,7 +39,7 @@ public class LoginController {
     @FXML
     private AnchorPane root;
     @FXML
-    private Label lblError;
+    private BorderPane lblPane;
     @FXML
     private JFXButton btnLogin;
 
@@ -47,35 +49,41 @@ public class LoginController {
     private double xOffset = 0;
     private double yOffset = 0;
 
-
     public void login() throws SQLException, IOException {
-        loginManager.attemptLogin(txtUsername.getText(),txtPassword.getText());
+        //loginManager.attemptLogin(txtUsername.getText(),txtPassword.getText());
         User u = LoginManager.getCurrentUser();
-        if (u != null){
-            if (u.getUserRole() == UserType.Admin){
+        if (u != null) {
+            if (u.getUserRole() == UserType.Admin) {
                 Stage root1 = (Stage) root.getScene().getWindow();
 
                 Stage stage = new Stage();
                 FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/GUI/VIEW/AdminViews/AdminDashboard.fxml"));
+                fxmlLoader.setLocation(getClass().getResource("/GUI/VIEW/AdminDashboard.fxml"));
 
                 Scene scene = new Scene(fxmlLoader.load());
+
                 stage.initStyle(StageStyle.UNDECORATED);
                 stage.setScene(scene);
                 stage.show();
 
-                scene.setOnMousePressed(mouseEvent -> {
-                    xOffset = mouseEvent.getSceneX();
-                    yOffset = mouseEvent.getSceneY();
+                scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        xOffset = event.getSceneX();
+                        yOffset = event.getSceneY();
+                    }
                 });
 
-                scene.setOnMouseDragged(mouseEvent -> {
-                    stage.setX(mouseEvent.getScreenX() - xOffset);
-                    stage.setY(mouseEvent.getScreenY() - yOffset);
-                    stage.setOpacity(0.8f);
+                scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        stage.setX(event.getScreenX() - xOffset);
+                        stage.setY(event.getScreenY() - yOffset);
+                        stage.setOpacity(0.8f);
+                    }
                 });
 
-                scene.setOnMouseExited((event) -> {
+                scene.setOnMouseDragExited((event) -> {
                     stage.setOpacity(1.0f);
                 });
 
@@ -85,15 +93,30 @@ public class LoginController {
 
                 root1.close();
             }
-        }else{
-            timer.startTimer();
-            lblError.setText(timer.getTxt().getText());
-
-            timer.addNodeToDisable(btnLogin);
-
-            txtPassword.clear();
-            txtUsername.clear();
+            System.out.println("c:");
+        } else {
+            startTimer();
         }
+    }
+
+    /**
+     * Adds the label of the Timer Class, uses addNodeToDisable
+     * and Timer's .startTimer method
+     * also clears the password and username field
+     */
+    private void startTimer() {
+        timer.startTimer();
+        lblPane.getChildren().clear();
+        Label label = timer.getTimerLabel();
+
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setTextFill(Paint.valueOf("RED"));
+        lblPane.setCenter(label);
+
+        timer.addNodeToDisable(btnLogin);
+
+        txtPassword.clear();
+        txtUsername.clear();
     }
 
 
@@ -103,25 +126,23 @@ public class LoginController {
             if (keyEvent.getCode() == KeyCode.ENTER) {
                 login();
             }
-        }else{
-            lblError.setText(timer.getTxt().getText());
-
+        } else {
+            startTimer();
         }
     }
 
-    public void minimize(){
+    public void minimize() {
         Stage stage = (Stage) root.getScene().getWindow();
         stage.setIconified(true);
     }
 
 
-    public void maximize(){
+    public void maximize() {
 
     }
 
-    public void exit(){
+    public void exit() {
         System.exit(0);
     }
-
 
 }
