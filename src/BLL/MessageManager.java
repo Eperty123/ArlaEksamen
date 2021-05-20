d80be52205b6a870e027e6966029584f88d82368
DPC-Laptop <dnisp@live.dk>
2021-05-20 10:15:00 +0200

    some changes

--- src/BLL/MessageManager.java
+++ src/BLL/MessageManager.java
@@ -46,8 +46,4 @@ public class MessageManager {
     public void loadScreenBitsMessages(ScreenBit screen) {
         messageDAL.loadScreenBitsMessages(screen);
     }
-
-    public void deleteMessage(Message message) {
-        messageDAL.deleteMessage(message);
-    }
 }
