b545dfe3c9dbc90e542b16ca7c3d845057be49d9
Onii-chan <failer007@live.dk>
2021-05-20 12:16:16 +0200

    Fixed StackedAreaChart missing mock data, deleted duplicate methods

--- src/BLL/MessageManager.java
+++ src/BLL/MessageManager.java
@@ -47,7 +47,4 @@ public class MessageManager {
         messageDAL.loadScreenBitsMessages(screen);
     }
 
-    public void deleteMessage(Message message) {
-        messageDAL.deleteMessage(message);
-    }
 }
