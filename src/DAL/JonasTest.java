fdfa204f43183e6b93570cc389de23f7941f43d7
jbn108 <jbuus9@gmail.com>
2021-05-24 09:58:24 +0200

    more dal work

--- src/DAL/JonasTest.java
+++ src/DAL/JonasTest.java
@@ -4,13 +4,21 @@ import BE.Message;
 import BE.MessageType;
 import BE.ScreenBit;
 import BE.User;
+import DAL.Parser.CSVParser;
 import DAL.Parser.UserBackUp;
 import GUI.Model.ScreenModel;
 import GUI.Model.UserModel;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import javafx.scene.paint.Color;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -41,13 +49,29 @@ public class JonasTest {
 
 
 
-        userBackUp.backUpUsers(users);
+        try (CSVReader reader = new CSVReader(new FileReader("src/Resources/MOCK_USERS2.csv"))) {
+            List<String[]> r = reader.readAll();
+            r.forEach(x -> System.out.println(Arrays.toString(x)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvException e) {
+            e.printStackTrace();
+        }
 
 
 
 
 
 
+
+
+
+
+    }
+
+    public void addUsersFromCSV(File csvFile){
+        List<String[]> rows = CSVParser.parseFile(csvFile.getAbsolutePath()).getParsedData();
+        List<User> users = new ArrayList<>();
 
     }
 
