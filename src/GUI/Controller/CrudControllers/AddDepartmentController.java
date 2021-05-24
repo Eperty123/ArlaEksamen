bcfa6a0c8c0e3a9b8df78884eaf8db043521b86f
DennisPC-bit <denn062g@easv365.dk>
2021-05-24 16:23:21 +0200

    a lot of work with department

---
+++ src/GUI/Controller/CrudControllers/AddDepartmentController.java
@@ -0,0 +1,114 @@
+package GUI.Controller.CrudControllers;
+
+import BE.Department;
+import BE.SceneMover;
+import BLL.DepartmentManager;
+import GUI.Controller.DPT.DepartmentViewController;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddDepartmentController implements Initializable {
+    @FXML
+    private Label signifierLabel;
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private JFXTextField txtDepartmentName;
+    @FXML
+    private JFXComboBox<Department> dptSuperior;
+    @FXML
+    private TableView<Department> subDptTable;
+    @FXML
+    private TableColumn<Department, String> subDepartments;
+    @FXML
+    private JFXComboBox<Department> subDpt;
+    private DepartmentManager departmentManager = new DepartmentManager();
+    private boolean isEdit = false;
+    private Department department;
+    private SceneMover sceneMover = new SceneMover();
+
+    public void setDepartment(Department department) {
+        signifierLabel.setText(String.format("Edit department (%s)", department.getName()));
+        this.department = department;
+        txtDepartmentName.setText(department.getName());
+        subDpt.setItems(departmentManager.getAllDepartments());
+        subDptTable.setItems(department.getAllSubDepartments());
+        if (department.isSubDepartment()) {
+            departmentManager.getAllDepartments().forEach(d -> {
+                    if (d.getAllSubDepartments().contains(department)) {
+                        if (!dptSuperior.getItems().contains(d))
+                            dptSuperior.getItems().add(d);
+                        dptSuperior.getSelectionModel().select(d);
+                    }
+            });
+        }
+        isEdit = true;
+    }
+
+
+    public void addSubDpt() {
+        if (!subDptTable.getItems().contains(subDpt.getSelectionModel().getSelectedItem()))
+            subDptTable.getItems().add(subDpt.getSelectionModel().getSelectedItem());
+    }
+
+    public void RemoveSubDpt() {
+        subDptTable.getItems().remove(subDptTable.getSelectionModel().getSelectedItem());
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void handleSave() {
+        if (!isEdit) {
+            department = new Department(-1, txtDepartmentName.getText());
+            department.getSubDepartments().clear();
+            department.getSubDepartments().addAll(subDptTable.getItems());
+            departmentManager.getAllDepartments().add(department);
+            departmentViewController.addSubDepartment(department);
+        } else {
+            department.setName(txtDepartmentName.getText());
+            department.getSubDepartments().clear();
+            department.getSubDepartments().addAll(subDptTable.getItems());
+            departmentManager.editDepartment(department);
+        }
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.close();
+    }
+
+    public void handleCancel() {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dptSuperior.setItems(departmentManager.getSuperDepartments());
+        subDepartments.setCellValueFactory(data -> data.getValue().nameProperty());
+        dptSuperior.getSelectionModel().selectedItemProperty().addListener((observableValue, oldDepartment, newDepartment) -> {
+            subDpt.getItems().add(oldDepartment);
+            subDpt.getItems().remove(newDepartment);
+            subDptTable.getItems().remove(newDepartment);
+        });
+        departmentManager.getAllDepartments().forEach(d->{
+            if(d.isSubDepartment() && !subDptTable.getItems().contains(d))
+                subDpt.getItems().add(d);
+        });
+    }
+
+    private DepartmentViewController departmentViewController;
+
+    public void setDepartmentViewController(DepartmentViewController departmentViewController) {
+        this.departmentViewController=departmentViewController;
+    }
+}
--- src/GUI/Controller/DPT/DepartmentViewController.java
+++ src/GUI/Controller/DPT/DepartmentViewController.java
@@ -1,26 +1,51 @@
 package GUI.Controller.DPT;
 
 import BE.Department;
+import BE.SceneMover;
 import BE.User;
+import BLL.DepartmentManager;
+import GUI.Controller.CrudControllers.AddDepartmentController;
+import GUI.Controller.PopupControllers.ConfirmationController;
 import com.jfoenix.controls.JFXTextField;
-import javafx.beans.property.SimpleStringProperty;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
 public class DepartmentViewController implements Initializable {
+    @FXML
+    private BorderPane manageIconsBP;
+    @FXML
+    private MaterialDesignIconView hideIcon;
+    @FXML
+    private AnchorPane superAC;
+    @FXML
+    private MaterialDesignIconView removeIcon;
+    @FXML
+    private MaterialDesignIconView settingsIcon;
     @FXML
     private BorderPane superBP;
     @FXML
@@ -34,20 +59,101 @@ public class DepartmentViewController implements Initializable {
     @FXML
     private TableColumn<User, Integer> phoneField;
     private Department department;
+    private ObservableList<User> users = FXCollections.observableArrayList();
+    private ObservableList<Department> subDepartments = FXCollections.observableArrayList();
+    private HBox hBox = new HBox();
+    private boolean isHidden = false;
+    private List<Node> deadChildren = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        hideIcon.setIcon(MaterialDesignIcon.MINUS_BOX);
+        settingsIcon.setIcon(MaterialDesignIcon.SETTINGS);
+        removeIcon.setIcon(MaterialDesignIcon.CLOSE_BOX);
+        settingsIcon.setOnMouseClicked(v -> openDepartmentModifyWindow());
         nameField.setCellValueFactory(data -> data.getValue().getFullNameProperty());
         emailField.setCellValueFactory(data -> data.getValue().emailProperty());
         phoneField.setCellValueFactory(data -> data.getValue().phoneProperty());
+        initUserChangeListener();
+        hideIcon.setOnMouseClicked((v) -> {
+            if (!isHidden) {
+                deadChildren.addAll(superAC.getChildren());
+                superAC.getChildren().clear();
+                hideIcon.setIcon(MaterialDesignIcon.PLUS_BOX);
+                superAC.getChildren().add(hideIcon);
+                isHidden = true;
+            } else {
+                superAC.getChildren().clear();
+                superAC.getChildren().addAll(deadChildren);
+                deadChildren.clear();
+                manageIconsBP.setLeft(hideIcon);
+                isHidden = false;
+            }
+        });
+        removeIcon.setOnMouseClicked((v) -> {
+            //TODO make some confirmation
+            DepartmentManager departmentManager = new DepartmentManager();
+            departmentManager.removeDepartment(department);
+            superAC.getChildren().clear();
+        });
+    }
+
+    private void openDepartmentModifyWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/CRUDViews/AddDepartment.fxml"));
+            AnchorPane pane = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(pane));
+            AddDepartmentController con = loader.getController();
+            con.setDepartment(department);
+            SceneMover sceneMover = new SceneMover();
+            sceneMover.move(stage, pane);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private AddDepartmentController openDepartmentAddWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/CRUDViews/AddDepartment.fxml"));
+            AnchorPane pane = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(pane));
+            AddDepartmentController con = loader.getController();
+            SceneMover sceneMover = new SceneMover();
+            sceneMover.move(stage, pane);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.show();
+            return con;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private void initUserChangeListener() {
+        users.addListener((ListChangeListener<? super User>) change -> {
+            change.next();
+            if (change.getAddedSize() > 0)
+                change.getAddedSubList().forEach(u -> dptUsersTable.getItems().add(u));
+            if (change.getRemovedSize() > 0)
+                change.getRemoved().forEach(u -> dptUsersTable.getItems().remove(u));
+        });
     }
 
     public void setDepartment(Department department) {
         this.department = department;
+        this.users = department.getUsers();
+        this.subDepartments = department.getSubDepartments();
         dptUsersTable.setItems(department.getUsers());
+
+        department.nameProperty().addListener((observableValue, s, t1) -> dptNameField.setText(t1));
         dptNameField.setText(department.getName());
-        HBox hBox = new HBox();
         department.getSubDepartments().forEach(d -> {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/DPT/DepartmentView.fxml"));
             try {
@@ -60,7 +166,28 @@ public class DepartmentViewController implements Initializable {
             }
 
         });
+        Button addSubDepartmentButton = new Button("Add sub department");
+        hBox.getChildren().add(addSubDepartmentButton);
+        addSubDepartmentButton.setOnMouseClicked((v) -> addSubDepartment());
         hBox.setAlignment(Pos.TOP_CENTER);
         superBP.setCenter(hBox);
+    }
+
+    public void addSubDepartment(Department subDepartment) {
+        subDepartments.add(subDepartments.size(), subDepartment);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/DPT/DepartmentView.fxml"));
+        try {
+            Node node = loader.load();
+            DepartmentViewController con = loader.getController();
+            con.setDepartment(subDepartment);
+            hBox.getChildren().add(hBox.getChildren().size() - 1, node);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addSubDepartment() {
+        AddDepartmentController addDepartmentController = openDepartmentAddWindow();
+        addDepartmentController.setDepartmentViewController(this);
     }
 }
