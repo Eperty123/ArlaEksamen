2ade347b30bfc1225fd685b0fcd164ff4034c75b
Onii-chan <failer007@live.dk>
2021-05-25 14:44:06 +0200

    Fixed exportPhoneNumbers()

--- test/PhoneListExportTest.java
+++ test/PhoneListExportTest.java
@@ -6,16 +6,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.function.Executable;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PhoneListExportTest {
     @DisplayName("Phone Number Export Test")
     @org.junit.jupiter.api.Test
     public void sendTest() {
+        var departmentList = new ArrayList<Department>();
         var department = new Department(1, "Department Test");
+        var department1 = new Department(1, "Administration");
         department.getUsers().add(UserModel.getInstance().getUser("admtest"));
-        System.out.println(department.getUsers().size());
+        department1.getUsers().add(UserModel.getInstance().getUser("mgrtest"));
+
+        departmentList.add(department);
+        departmentList.add(department1);
+
         Assertions.assertDoesNotThrow(() -> {
-            DepartmentModel.getInstance().exportPhoneNumbers(department);
+            DepartmentModel.getInstance().exportPhoneNumbers(departmentList);
+            System.out.println(department.getUsers().size());
         });
     }
 }
