aaa511dd31a24fe3eaffb9ab066594452222481e
jbn108 <jbuus9@gmail.com>
2021-05-27 10:56:07 +0200

    Fixed export telephone bug + manager message bug

--- src/DAL/JonasTest.java
+++ src/DAL/JonasTest.java
@@ -9,6 +9,7 @@ import GUI.Model.UserModel;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import javafx.scene.paint.Color;
+import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
 
 import java.io.File;
 import java.io.FileReader;
@@ -37,13 +38,22 @@ public class JonasTest {
         List<ScreenBit> screenBits = ScreenModel.getInstance().getAllScreenBits();
         List<Department> departments = departmentDAL.getDepartments();
 
+        /*
         try(Connection con = dbCon.getConnection()){
-
             for(ScreenBit s : screenBits){
                 screenDAL.createScreenBitTimeTable(con, s.getId());
             }
-
         }
+
+         */
+        int slots = 17;
+
+
+        int halfHours = slots % 2;
+        int hours = (slots - halfHours) / 2;
+
+        System.out.println("Duration: " + hours + " hours and " + halfHours * 30 + " minutes.");
+
     }
 
 
